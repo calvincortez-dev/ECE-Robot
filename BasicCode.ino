bf0818154290f1ac70df9d4aaf7ba6472f0b423f
Yiska08 <61596105+Yiska08@users.noreply.github.com>
2025-10-31 11:26:24 -0700

    declare 3 arrays (min,max,weighted) from ir calibration sensor

--- BasicCode.ino
+++ BasicCode.ino
@@ -14,15 +14,22 @@
 
 uint16_t sensorValues[8];
 
+//left wheel pins declaration
 const int left_nslp_pin=31; // nslp ==> awake & ready for PWM
 const int left_dir_pin=29;
 const int left_pwm_pin=40;
 
+//right wheel pins declaration
 const int right_nslp_pin = 11;
 const int right_dir_pin = 30;
 const int right_pwm_pin = 39;
 
 const int LED_RF = 41;
+
+//Calibration Values for IR Sensor
+long sensorMin[8] = {872, 710, 756, 687, 730, 688, 780, 849};
+long sensorMax[8] = {1628, 1790, 1744, 1514, 1447, 1812, 1720, 1651};
+long weights[8] = {-15, -14, -12, -8, 8, 12, 14, 15}; //Chose -15 -14 -12 -8 weighted sum scheme
 
 ///////////////////////////////////
 void setup() {
